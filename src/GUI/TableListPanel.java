ef742ed11d953432b706deb2187ed803d00b217e
이승진 <fewfree12@naver.com>
2015-05-18 02:31:18 +0900

    쉬었다.

--- src/GUI/TableListPanel.java
+++ src/GUI/TableListPanel.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
@@ -37,7 +38,7 @@ public class TableListPanel extends JPanel implements ActionListener
 	TableListPanel(PosData data)
 	{
 		setData(data);
-		Table = new JButton[50];
+		Table = new JButton[24];
 		setTableNum(getData().getTableList().size());
 		
 		lineBorder = new LineBorder(Color.BLACK, 3);
@@ -52,9 +53,9 @@ public class TableListPanel extends JPanel implements ActionListener
 	public void printTableList(int TableNum)
 	{
 		int j = 0;
-		for(int i= 0;i+j*8<TableNum; i++ )
+		for(int i= 0;i+j*6<TableNum; i++ )
 		{
-			if(i%8 == 0&& i !=0)
+			if(i%6 == 0&& i !=0)
 			{
 				j += 1;
 				i= 0;
@@ -64,19 +65,20 @@ public class TableListPanel extends JPanel implements ActionListener
 	}
 	
 	public void printTable(int i,int j)
-	{
-			Table[i+j*8] = new JButton(""+(i+j*8+1));
-			Table[i+j*8].setBackground(Color.WHITE);
-			Table[i+j*8].setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 14));
-			Table[i+j*8].setBorder(lineBorder);
-			Table[i+j*8].addActionListener(this);
-			this.add(Table[i+j*8]).setBounds(10 + i*97, 10+j*150, 80, 120);
+	{	
+		Table[i+j*6] = new JButton("<html>"+"<p align = center>" + (i + j * 6 + 1)+"</p>" +"<br>±Ý¾×: " + (double)getData().getTableList().get(i+j*6).getTotalPrice() /10000 + "</html>");
+		Table[i+j*6].setLayout(null);
+		Table[i+j*6].setBackground(Color.WHITE);
+		Table[i+j*6].setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 17));
+		Table[i+j*6].setBorder(lineBorder);
+		Table[i+j*6].addActionListener(this);
+		this.add(Table[i+j*6]).setBounds(10 + i*128, 10+j*150, 120, 120);
 
 	}
 
 	public void addTableList()
 	{
-		if(tableNum>=32)
+		if(tableNum >= 24)
 		{
 			System.out.println("Table is full");
 		}
@@ -92,7 +94,7 @@ public class TableListPanel extends JPanel implements ActionListener
 			Tcon.addTable(data.getTableList(), newTable);
 			
 			int i=0,j=0;
-			for(i = tableNum;i >8;i -=8)
+			for(i = tableNum;i >6;i -=6)
 			{
 				j ++;
 			}
